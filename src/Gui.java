8ef264539d5037d02bc7f47ada04608c0be96ad5
Suhas-Neramya <neramya123@gmail.com>
2023-03-01 12:35:38 +0530

    Encryption Code Completed

--- src/Gui.java
+++ src/Gui.java
@@ -1,7 +1,19 @@
+import Elements.RoundBorder;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
 import javax.swing.*;
 import java.awt.*;
-
-import Elements.RoundBorder;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.time.LocalDateTime;
 
 public class Gui extends JFrame {
 
@@ -12,6 +24,7 @@ public class Gui extends JFrame {
         setLocationRelativeTo(null);
         setLayout(null);
         setVisible(true);
+
 
 
         JButton encryptData = new JButton("Encrypt Data");
@@ -28,6 +41,11 @@ public class Gui extends JFrame {
         decryptData.setFocusable(false);
         decryptData.setBorder(new RoundBorder(10, "#312f1e"));
         this.add(decryptData);
+
+        JLabel logo = new JLabel(new ImageIcon("src\\images\\logo.png"));
+        logo.setBounds(-50, 50, 600, 250);
+        this.add(logo);
+
 
 
         encryptData.addActionListener(e -> {
@@ -62,8 +80,9 @@ public class Gui extends JFrame {
 
             JTextField key = new JTextField();
             key.setBounds(170, 100, 250, 30);
-            key.setFocusable(false);
+            key.setEnabled(true);
             this.add(key);
+
 
 
             JButton encrypt = new JButton("Encrypt");
@@ -72,6 +91,120 @@ public class Gui extends JFrame {
             encrypt.setFocusable(false);
             encrypt.setBorder(new RoundBorder(10, "#312f1e"));
             this.add(encrypt);
+
+            JButton goBack = new JButton("Go Back");
+            goBack.setBounds(275, 450, 150, 40);
+            goBack.setBackground(Color.decode("#fff39d"));
+            goBack.setFocusable(false);
+            goBack.setBorder(new RoundBorder(10, "#312f1e"));
+            this.add(goBack);
+
+            JLabel imagePath = new JLabel();
+            imagePath.setBounds(50, 200, 400, 30);
+            imagePath.setFocusable(false);
+
+
+
+            JLabel browseText = new JLabel("Browse the file");
+            browseText.setBounds(50, 300, 150, 30);
+            browseText.setFocusable(false);
+            this.add(browseText);
+
+            JButton browse = new JButton("Browse");
+            browse.setBounds(150, 300, 150, 40);
+            browse.setBackground(Color.decode("#fff39d"));
+            browse.setFocusable(false);
+            browse.setBorder(new RoundBorder(10, "#312f1e"));
+            this.add(browse);
+
+
+            browse.addActionListener(e -> {
+                JFileChooser fileChooser = new JFileChooser();
+                fileChooser.setCurrentDirectory(new java.io.File("."));
+                fileChooser.setDialogTitle("Select the image");
+                fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+                fileChooser.setAcceptAllFileFilterUsed(false);
+                if (fileChooser.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
+                    imagePath.setText(fileChooser.getSelectedFile().getAbsolutePath());
+                }
+            });
+
+
+            encrypt.addActionListener(e -> {
+                String enkey = key.getText();
+                if (enkey.length()<=24){
+                    enkey= enkey+"000000000000000000000000";
+                    enkey = enkey.substring(0, 24);
+                }else {
+                    enkey = enkey.substring(0, 24);
+                }
+                FileInputStream inFile;
+                FileOutputStream outFile;
+                try {
+                    inFile = new FileInputStream(imagePath.getText());
+                    outFile = new FileOutputStream("./src/EncryptedImages/"+LocalDateTime.now().toString().replace(".","").replace(":","") +".png");
+                    Key secretKey = new SecretKeySpec(enkey.getBytes(), "AES");
+                    Cipher cipher = Cipher.getInstance("AES");
+                    cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+                    byte[] input = new byte[64];
+                    int bytesRead;
+                    while ((bytesRead = inFile.read(input)) != -1) {
+                        byte[] output = cipher.update(input, 0, bytesRead);
+                        outFile.write(output);
+                    }
+                    byte[] output = cipher.doFinal();
+                    outFile.write(output);
+                    inFile.close();
+                    outFile.flush();
+                    outFile.close();
+                    System.out.println("File Encrypted.");
+                } catch (NoSuchAlgorithmException | NoSuchPaddingException
+                         | InvalidKeyException | IOException
+                         | IllegalBlockSizeException | BadPaddingException EX) {
+                    System.out.println(EX.getMessage());
+                }
+            });
+
+
+
+            goBack.addActionListener(e -> {
+                new Gui();
+                this.dispose();
+            });
+
+
+
+
+
+        }
+    }
+
+
+    public static class DecryptData extends JFrame {
+        public DecryptData() {
+            super("Encrypto  - Decryption");
+            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            setSize(500, 700);
+            setLocationRelativeTo(null);
+            setLayout(null);
+            setVisible(true);
+
+            JLabel text = new JLabel("Enter the key code ");
+            text.setBounds(50, 100, 150, 30);
+            text.setFocusable(false);
+            this.add(text);
+
+            JTextField key = new JTextField();
+            key.setBounds(170, 100, 250, 30);
+            this.add(key);
+
+
+            JButton decrypt = new JButton("Decrypt");
+            decrypt.setBounds(75, 450, 150, 40);
+            decrypt.setBackground(Color.decode("#fff39d"));
+            decrypt.setFocusable(false);
+            decrypt.setBorder(new RoundBorder(10, "#312f1e"));
+            this.add(decrypt);
 
             JButton goBack = new JButton("Go Back");
             goBack.setBounds(275, 450, 150, 40);
@@ -94,26 +227,15 @@ public class Gui extends JFrame {
             this.add(browse);
 
 
-            encrypt.addActionListener(e -> {
-                //encryption Code
+            decrypt.addActionListener(e -> {
+
             });
 
+            goBack.addActionListener(e -> {
+                new Gui();
+                this.dispose();
+            });
 
-
-
-
-        }
-    }
-
-
-    public static class DecryptData extends JFrame {
-        public DecryptData() {
-            super("Encrypto  - Decryption");
-            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            setSize(500, 700);
-            setLocationRelativeTo(null);
-            setLayout(null);
-            setVisible(true);
         }
     }
 
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import java.util.UUID;
+
 public class Main {
     public static void main(String[] args) {
-
+        String uuid = UUID.randomUUID().toString().replace("-", "").substring(0, 8);
+        System.out.println(uuid);
     }
 }
